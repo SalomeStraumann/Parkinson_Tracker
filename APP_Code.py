552f203eb871f4e347896a83a52fe4fe66a2bc6c
parkinson-tracker-app <131356614+SalomeStraumann@users.noreply.github.com>
2023-05-31 14:23:51 +0200

    Update APP_Code.py

--- APP_Code.py
+++ APP_Code.py
@@ -102,7 +102,7 @@ if selected_symptoms:
             st.write(f'- {symptom}: {severity_level}')
             symptoms_and_severity = {symptom: severity_levels[symptom] for symptom in selected_symptoms if symptom != 'Keine Symptome'}
         else:
-            symptoms_and_severity = {symptom in selected_symptoms}
+            symptoms_and_severity = symptom in selected_symptoms
 else:
     st.write('Keine Symptome ausgewählt')
 
